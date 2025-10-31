1b1872f7faa29fa2cf4f99448a7735f2cabda3d0
Natalie Rekai <ncrekai@gmail.com>
2025-10-31 14:12:15 -0400

    return base-profs

---
+++ app/lib/base/base-proficiencies.js
@@ -0,0 +1,21 @@
+class BaseProficiencies {
+   constructor() {}
+   
+   armour = [];
+   weapons = [];
+   tools = [];
+   savingThrows = [];
+   skills = [];
+   languages = [];
+
+   selectFromList = {
+      armour: null,
+      weapons: null,
+      tools: null,
+      savingThrows: null,
+      skills: null,
+      languages: null,
+   }
+}
+
+export default BaseProficiencies;
